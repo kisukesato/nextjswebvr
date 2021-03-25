064967a41d3ee78561271737bd859544e5ac4c56
kisukesato <65474365+kisukesato@users.noreply.github.com>
2021-03-25 11:32:07 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,11 @@ export default function Home() {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,intial-scale=1">
         <title>WebAR</title>
+　　　   <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
+        <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
     </Head>
     <body style="margin:0px; overflow:hidden;">
     　　<NextScript />
-        <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
-        <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-
         <a-scene embedded arjs="debugUIEnabled:false; sourceType: webcam;">
             <a-marker preset="custom" type='pattern' url="my-icon-marker.patt">
               <a-text value="My name is soeyu!\n Nice to meet you!" position=" 0 0 1" align="center" rotation="-90 0 0" color="#7993ff">
