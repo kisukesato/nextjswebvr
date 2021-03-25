0c081759623da394a33094467246407b54c16689
kisukesato <65474365+kisukesato@users.noreply.github.com>
2021-03-25 12:14:51 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <Head>
-        <title>WebAR</title>
-　　　   <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
-        <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-    </Head>
-    <a-scene embedded arjs="debugUIEnabled:false; sourceType: webcam;">
-            <a-marker preset="custom" type='pattern' url="my-icon-marker.patt">
-              <a-text value="My name is soeyu!\n Nice to meet you!" position=" 0 0 1" align="center" rotation="-90 0 0" color="#7993ff"></a-text>
-            </a-marker>
-            <a-entity camera></a-entity>
-    </a-scene>
+    <div>
+      <iframe src="index.html" frameborder="0" id="filecontainer"></iframe>
+    </div>
   )
 }
