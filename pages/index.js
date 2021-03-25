476273f835c447a80b6f2511f2c794b8d68291f9
kisukesato <65474365+kisukesato@users.noreply.github.com>
2021-03-25 12:02:18 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,11 @@ export default function Home() {
 　　　   <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
         <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
     </Head>
-    <div>
-        <a-scene embedded arjs="debugUIEnabled:false; sourceType: webcam;">
+    <a-scene embedded arjs="debugUIEnabled:false; sourceType: webcam;">
             <a-marker preset="custom" type='pattern' url="my-icon-marker.patt">
               <a-text value="My name is soeyu!\n Nice to meet you!" position=" 0 0 1" align="center" rotation="-90 0 0" color="#7993ff"></a-text>
             </a-marker>
             <a-entity camera></a-entity>
-        </a-scene>
-    </div>
+    </a-scene>
   )
 }
