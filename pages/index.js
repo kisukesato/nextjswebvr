5c87103570712ab21c4181c160df11c59c5048d3
kisukesato <65474365+kisukesato@users.noreply.github.com>
2021-03-25 13:08:59 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-import Head from 'next/head'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-        <iframe
-            width='100%'
-            id='iframe'
-            src='https://github.com/kisukesato/nextjswebvr/blob/e8e8e78f91ea9af7883cf9f321db0e58e3f7cedd/pages/index.html'
-        ></iframe>
+<Html>
+    <Head>
+        <title>WebAR</title>
+        <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
+        <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+    </Head>
+    <body style="margin:0px; overflow:hidden;">
+        <a-scene embedded arjs="debugUIEnabled:false; sourceType: webcam;">
+            <a-marker preset="custom" type='pattern' url="my-icon-marker.patt">
+              <a-text value="My name is soeyu!\n Nice to meet you!" position=" 0 0 1" align="center" rotation="-90 0 0" color="#7993ff">
+              </a-text>
+            </a-marker>
+            <a-entity camera></a-entity>
+        </a-scene>
+    </body>
+</Html>
   )
 }
