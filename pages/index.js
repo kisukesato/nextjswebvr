6306b1538ca16a2dfce7c2cddf7b199c2d0a5f85
kisukesato <65474365+kisukesato@users.noreply.github.com>
2021-03-25 12:17:27 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <iframe src="index.html" frameborder="0" id="filecontainer"></iframe>
+      <iframe src="./index.html" frameborder="0" id="filecontainer"></iframe>
     </div>
   )
 }
